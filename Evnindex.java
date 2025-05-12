73f770508943176618b8ec9f06bcdd857560e59a
JimsonR <jimsonjimmy2008@gmail.com>
2025-05-12 16:22:19 +0530

    commit

--- Evnindex.java
+++ Evnindex.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Evnindex {
     public static void main(String args[]) throws IOException {
         int[] ind = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
+        List<Integer> arr = new ArrayList<>();
         for(int i = 0;i<ind.length;i++){
 //              if(i % 2 == 0) {
 //                  System.out.println(ind[i]);
